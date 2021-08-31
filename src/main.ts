a955c9abf2e3c758a21135af8fb6186b053b573f
Julian Stang <julian.stang@tum.de>
2021-08-31 08:45:21 +0200

    Added events

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import './main.css';
 
-let dateTime = new Date();
+// let event: Event = new Event();
+let groupMilestoneColor: string = '#99b7b0';
+let codeProjectMilestoneColor: string = '#41c54a';
+let thesisProjectMilestoneColor: string = '#1d3bd2';
+
 
 document.addEventListener('DOMContentLoaded', function () {
     let calendarEl: HTMLElement = document.getElementById('calendar')!;
@@ -23,15 +27,15 @@ document.addEventListener('DOMContentLoaded', function () {
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
         },
-        locale: 'en-us',
-        firstDay: 1,
+        locale: 'en-us', // us english locale
+        firstDay: 1, // monday as first day
         // initialDate: '2021-09-01',
         navLinks: true, // can click day/week names to navigate views
         editable: true,
         dayMaxEvents: true, // allow "more" link when too many events
-        weekNumbers: true,
+        weekNumbers: true, // show number of week
         businessHours: {
-            daysOfWeek: [1, 2, 3, 4, 5],
+            daysOfWeek: [1, 2, 3, 4, 5], // monday - friday
             startTime: '08:00',
             endTime: "18:00",
         },
@@ -42,6 +46,41 @@ document.addEventListener('DOMContentLoaded', function () {
         dayHeaderContent(arg: DayHeaderContentArg) {
             return createElement(CustomDayHeader, { text: arg.text })
         },
+        events: [
+            {
+                id: '1',
+                title: 'Master Thesis',
+                start: '2021-09-01',
+                end: '2022-02-28',
+                backgroundColor: groupMilestoneColor,
+                display: 'background',
+                url: 'https://gitlab.lrz.de/groups/aerospace-master-thesis/-/milestones/1'
+            },
+            {
+                id: '2',
+                title: 'Implement Neural ODE',
+                start: '2021-09-01',
+                end: '2021-09-30',
+                url: 'https://gitlab.lrz.de/aerospace-master-thesis/aerospace-thesis-code/-/milestones/1',
+                backgroundColor: codeProjectMilestoneColor,
+            },
+            {
+                id: '3',
+                title: 'Exposé',
+                start: '2021-09-01',
+                end: '2021-10-31',
+                backgroundColor: thesisProjectMilestoneColor,
+                url: 'https://gitlab.lrz.de/groups/aerospace-master-thesis/-/milestones/2'
+            },
+            {
+                id: '4',
+                title: 'Basic Understanding of AutoDiff',
+                start: '2021-09-01',
+                end: '2021-09-13',
+                backgroundColor: thesisProjectMilestoneColor,
+                url: 'https://gitlab.lrz.de/groups/aerospace-master-thesis/-/milestones/3'
+            }
+        ],
     });
 
     calendar.render();
